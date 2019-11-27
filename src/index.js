cbcd46d16ed5f43d716604e0d673547ee311faa6
DannyBrito <britodanny1997@gmail.com>
2019-11-27 10:23:00 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,94 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+const imageId = 4057 //Enter the id from the fetched image here
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const likeURL = `https://randopic.herokuapp.com/likes/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-  let imageId = 1 //Enter the id from the fetched image here
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+let comments
+let likesCount = 0
+let spanLikes
+const imageCard = document.getElementById('image_card')
+function getImage(){
+    fetch(imageURL)
+      .then(res => res.json())
+      .then(json =>{
+        renderPage(json)
+        comments = document.getElementById('comments')
+        spanLikes = document.getElementById('likes')
+        json.comments.forEach(renderComment)
+      })
+}
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+const renderPage = obj =>{
+  likesCount = obj.like_count
+  const imagHTML = `<img src=${obj.url} id="image" data-id=${obj.id}/>
+  <h4 id="name">${obj.name}</h4>
+  <span>Likes:
+  <span id="likes">${likesCount}</span>
+  </span>
+  <button id="like_button" data-img-id=${obj.id}>Like</button>
+  <form id="comment_form" data-img=${obj.id}>
+    <input id="comment_input" type="text" name="comment" placeholder="Add Comment"/>
+    <input type="submit" value="Submit"/>
+  </form>
+  <ul id="comments">
+  </ul>
+  `
+  imageCard.innerHTML = imagHTML
+}
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+const renderComment = comment =>{
+    const cmHTML = `<li data-comment-id=${comment.id}> ${comment.content} </li>`
+    comments.innerHTML += cmHTML
+}
 
+imageCard.addEventListener('click', e =>{
+  e.preventDefault()
+  console.log(e.target)
+  if(e.target.id === "like_button"){ // check if like_button
+    updateLikes() // optimi.. approach
+    persistLike()
+  }
+  else if(e.target.value === "Submit"){
+    const comment = document.getElementById('comment_input')
+    optCommentRender(comment.value)
+    persistComment(comment.value)
+    comment.parentNode.reset()
+  }
 })
+
+const updateLikes = () =>{
+    spanLikes.innerHTML = parseInt(spanLikes.innerHTML) + 1
+}
+
+const persistLike = () =>{
+  fetch(likeURL,{
+    method: 'POST',
+    headers:{
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      image_id: imageId
+    })})   // optim.. approach
+}
+
+const optCommentRender = comment =>{
+  const cmHTML = `<li> ${comment} </li>`
+    comments.innerHTML += cmHTML
+}
+
+const persistComment = (commentContent) => {
+  fetch(commentsURL,{
+    method: 'POST',
+    headers:{
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      image_id: imageId,
+      content: commentContent
+    })})   // optim.. approach
+}
+//
+getImage()
