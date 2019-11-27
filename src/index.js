dd1bc400fea987ddce9bb868f896fda132feba52
github username <github email address>
2019-11-27 11:02:07 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4062 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+
+fetch("https://randopic.herokuapp.com/images/4062")
+  .then(resp => resp.json())
+  .then(image => {
+    console.log(image)
+  // image.innerHTML = ${image.name}
+  
+
+
+
+    
+  }) 
+
+
+
+
 
 })
